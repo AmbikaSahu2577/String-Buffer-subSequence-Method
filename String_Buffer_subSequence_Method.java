cfc67d10f592bba11036e3cfadc26cdcf0f95060
Ambika Sahu <90752801+AmbikaSahu2577@users.noreply.github.com>
2022-11-21 16:46:24 +0530

    Add files via upload

---
+++ String_Buffer_subSequence_Method.java
@@ -0,0 +1,8 @@
+class String_Buffer_subSequence_Method
+{
+	public static void main(String[] args) 
+	{
+		StringBuffer sb = new StringBuffer("javm progrraming");
+		System.out.println(sb.subSequence(3,7));
+	}
+}
